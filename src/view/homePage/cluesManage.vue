de8d841870f734b06b38d11f9a6d0d7335b09d77
ndx2527 <1378362527@qq.com>
2022-07-04 22:35:57 +0800

    fix：md文档

--- src/view/homePage/cluesManage.vue
+++ src/view/homePage/cluesManage.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    线索管理
   </div>
-
 </template>
 
 <script setup>
-import { reactive, computed } from "vue";
+import { getCurrentInstance, onMounted, reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+let router = useRouter();
+const { proxy } = getCurrentInstance();
+const { query } = router.currentRoute.value;
+onMounted(() => {});
 </script>
 
-<style>
+<style lang='scss' scoped>
 </style>
